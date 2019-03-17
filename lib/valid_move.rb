cc8c7174b16d907211ed4ca77f9f6d85d183512a
Briana Cooke <br.cooke12@gmail.com>
2019-03-17 04:47:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,22 +1,19 @@
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+  if num.between?(0, 8) == true
     return true
   else
     return false
+    if (position_taken?(board, index)) == false && (on_board?(index) == true)
+      return true
+    else
+      return false
   end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
