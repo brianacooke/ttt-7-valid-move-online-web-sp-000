1f7865141b7171c89e34b861c60c555b3cba027b
Briana Cooke <br.cooke12@gmail.com>
2019-03-17 04:53:25 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,12 @@
 def valid_move?(board, position)
-	spot = position.to_i - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
-		false
- 	else
-		true
- 	end
- end
+  if position.to_i.between?(1,9)
+    if !position_taken?(board, position.to_i-1)
+      true
+    else
+      false
+    end
+  end
+
 
  def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
